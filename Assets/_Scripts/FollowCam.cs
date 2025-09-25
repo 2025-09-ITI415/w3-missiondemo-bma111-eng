933bebdedcbd183f2049bf3137e4c1ea1e76640a
bma111-eng <bma111@scarletmail.rutgers.edu>
2025-09-25 11:26:30 -0400

    Created follow cam scripts and multiple castles
    - Need to fix followcam reset upon firing from "both" view

--- Assets/_Scripts/FollowCam.cs
+++ Assets/_Scripts/FollowCam.cs
@@ -2,39 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCam : MonoBehaviour{
+public class FollowCam : MonoBehaviour
+{
+    static private FollowCam S;   // Another private Singleton
     static public GameObject POI;
+    public enum eView { none, slingshot, castle, both };
 
     [Header("Inscribed")]
     public float easing = 0.05f;
 
     public Vector2 minXY = Vector2.zero;
+    public GameObject viewBothGO;
 
     [Header("Dynamic")]
     public float camZ;
-
-    void Awake(){
-        camZ = this. transform.position.z;
+    public eView nextView = eView.slingshot;
+    void Awake()
+    {
+        camZ = this.transform.position.z;
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         Vector3 destination = Vector3.zero;
 
-        if(POI!= null){
+        if (POI != null)
+        {
 
             Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
-            if ((poiRigid != null) && poiRigid.IsSleeping()){
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
                 POI = null;
             }
         }
 
-        if(POI != null){
+        if (POI != null)
+        {
             destination = POI.transform.position;
         }
-    //    if(POI == null) return;
+        //    if(POI == null) return;
 
-    //    Vector3 destination = POI.transform.position;
-    
+        //    Vector3 destination = POI.transform.position;
+
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
 
@@ -45,5 +54,35 @@ public class FollowCam : MonoBehaviour{
         transform.position = destination;
 
         Camera.main.orthographicSize = destination.y + 10;
+    }
+    public void SwitchView(eView newView)
+    {                                  // f
+        if (newView == eView.none)
+        {
+            newView = nextView;
+        }
+        switch (newView)
+        {                                                   // g
+            case eView.slingshot:
+                POI = null;
+                nextView = eView.castle;
+                break;
+            case eView.castle:
+                POI = MissionDemolition.GET_CASTLE();                               // h
+                nextView = eView.both;
+                break;
+            case eView.both:
+                POI = viewBothGO;
+                nextView = eView.slingshot;
+                break;
+        }
+    }
+    public void SwitchView()
+    {                                                  // i
+        SwitchView(eView.none);
+    }
+    static public void SWITCH_VIEW(eView newView)
+    {                           // j
+        S.SwitchView(newView);
     }
 }
